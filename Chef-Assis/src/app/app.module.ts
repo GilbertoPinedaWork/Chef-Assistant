3bf882365c7042bec86b8768c0efa93c3ed8bf9d
Timothy Mendez Feliciano <timothy.mendez@upr.edu>
2019-04-11 21:34:26 -0400

    Routing done. Now to implement features for UI

--- Chef-Assis/src/app/app.module.ts
+++ Chef-Assis/src/app/app.module.ts
@@ -1,16 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponent } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { IngredientManagerComponent } from './components/ingredient-manager/ingredient-manager.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    IngredientManagerComponent
+    routingComponent,
   ],
   imports: [
     BrowserModule,
